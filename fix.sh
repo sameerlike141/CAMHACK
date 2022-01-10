9b30f94021ffe6b41b94587f722e994a248c30fc
anynomouscheetah <79717256+anynomouscheetah@users.noreply.github.com>
2022-01-10 15:53:31 +0500

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -2,7 +2,7 @@ printf "THIS SCRIPT SOLVED YOUR PROBLEM DON'T WORRY"
 sleep 2
 chmod 777 ngrok
 killall ngrok
-bash camhack.sh
+
 printf "YOUR PROBLEM HAS BEEN SOLVED LET'S FUCK YOUR TARGET" 
 sleep 2
 printf "INFOSEC HACKER ACADEMY | BILAL ALI" 
